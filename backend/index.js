8515c167f7a94c5d901345dfd05b3bd2ac1f2934
yogendra0101 <yogendra@0101digitall.com>
2024-07-02 08:29:46 +0530

    backend poushed

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
